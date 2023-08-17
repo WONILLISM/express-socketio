6adfd89b41d65117b24e950ff5602e2d731688fe
WONILLISM <wonillism@gmail.com>
2023-08-17 16:29:47 +0900

    feat: add socket events (#2)

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,12 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import {
+  ChatData,
+  ClientToServerEvents,
+  InterServerEvents,
+  ServerToClientEvents,
+} from "./interfaces/socket.io";
 
 const PORT = 4000;
 
@@ -8,7 +14,12 @@ const app: Express = express();
 
 const httpServer = createServer(app);
 
-const io = new Server(httpServer, {
+const io = new Server<
+  ClientToServerEvents,
+  ServerToClientEvents,
+  InterServerEvents,
+  ChatData
+>(httpServer, {
   cors: {
     origin: "http://localhost:5173",
   },
@@ -16,6 +27,32 @@ const io = new Server(httpServer, {
 
 io.on("connection", (socket) => {
   console.log(socket.id, "was connected");
+
+  socket.on("CREATE_ROOM", (data: ChatData) => {
+    console.log("Room created");
+
+    socket.emit("JOINED_ROOM", data);
+  });
+
+  socket.on("JOIN_ROOM", (data: ChatData) => {
+    console.log(`${data.username} enter the room.`);
+  });
+
+  socket.on("SEND_CELB_MESSAGE", (data: ChatData) => {
+    console.log(`${data.username} send message: ${data.message}`);
+
+    io.emit("RECIEVED_CELB_MESSAGE", data);
+  });
+
+  socket.on("SEND_FAN_MESSAGE", (data: ChatData) => {
+    console.log(`${data.username} send message: ${data.message}`);
+
+    socket.emit("RECIEVED_FAN_MESSAGE", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("A client disconnected.");
+  });
 });
 
 httpServer.listen(PORT, () => {
